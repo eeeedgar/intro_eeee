25ec9da1aef44c191b64e70f15da0e74cd16fdfd
eeeedgar <oakmillennial@gmail.com>
2022-08-23 21:26:49 +0300

    feat: add draggable text

--- lib/intro_page.dart
+++ lib/intro_page.dart
@@ -12,16 +12,36 @@ class IntroPage extends StatefulWidget {
 }
 
 class _IntroPageState extends State<IntroPage> {
-
+  Offset _offset = Offset.zero; // changed
 
   @override
   Widget build(BuildContext context) {
     return NavigationTemplate(
-      data: const Center(
-        child: Text('Hello, I am Edgar', style: TextStyle(fontSize: 100),),
+      data: GestureDetector(
+        onDoubleTap: () => setState(() => _offset = Offset.zero),
+        child: Container(
+          color: const Color(0x0f000f0f),
+          alignment: Alignment.center,
+          child: Transform(
+            transform: Matrix4.identity()
+              ..setEntry(3, 2, 0.001) // perspective
+              ..rotateX(0.01 * _offset.dy) // changed
+              ..rotateY(-0.01 * _offset.dx), // changed
+            alignment: FractionalOffset.center,
+            child: GestureDetector(
+              // new
+              onPanUpdate: (details) => setState(() => _offset += details.delta),
+              // onDoubleTap: () => setState(() => _offset = Offset.zero),
+              child: const Text(
+                'Hello, I am Edgar',
+                style: TextStyle(fontSize: 100),
+              ),
+            ),
+          ),
+        ),
       ),
-      up: () =>
-          Navigator.of(context).pushReplacement(getRoute(const BioPage(), 'up')),
+      up: () => Navigator.of(context)
+          .pushReplacement(getRoute(const BioPage(), 'up')),
       right: () => Navigator.of(context)
           .pushReplacement(getRoute(const GamePage(), 'right')),
     );
