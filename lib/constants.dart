a2f159d6d610fc752350cede967356ab6e378b34
eeeedgar <oakmillennial@gmail.com>
2022-08-24 06:20:58 +0300

    fix: use constants

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const backgroundBlue = Color(0xff1f1c9e);
+const fontYellow = Color(0xffe6f032);
+
+const greetingMessage = 'hello, my name is Edgar\nI am a Flutter-developer';
+const greetingQuestion = 'wanna know about me?';
--- lib/intro_running_text.dart
+++ lib/intro_running_text.dart
@@ -2,6 +2,7 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:intro_eeee/intro_page_card.dart';
 import 'package:intro_eeee/navigation_template.dart';
+import 'constants.dart';
 
 class IntroRunningText extends StatelessWidget {
   const IntroRunningText({Key? key}) : super(key: key);
@@ -9,27 +10,25 @@ class IntroRunningText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xff1f1c9e),
+      backgroundColor: backgroundBlue,
       body: Padding(
         padding:
             const EdgeInsets.only(left: 50, top: 100, right: 50, bottom: 100),
         child: Column(
-          // crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Flexible(
               flex: 3,
               child: Container(
-                // color: Colors,
                 alignment: Alignment.center,
                 child: AnimatedTextKit(
                   isRepeatingAnimation: false,
                   animatedTexts: [
                     TyperAnimatedText(
-                        'hello, my name is Edgar\nI am a Flutter-developer',
+                        greetingMessage,
                         curve: Curves.slowMiddle,
                         textStyle: const TextStyle(
                           fontFamily: 'FeatureMono',
-                          color: Color(0xffe6f032),
+                          color: fontYellow,
                           fontSize: 50,
                         )),
                   ],
@@ -40,17 +39,16 @@ class IntroRunningText extends StatelessWidget {
               flex: 2,
               child: Container(
                 alignment: Alignment.center,
-                // color: Colors.red,
                 child: GestureDetector(
                   child: TextLiquidFill(
                     loadDuration: const Duration(seconds: 2),
-                    text: 'wanna know about me?',
+                    text: greetingQuestion,
                     textAlign: TextAlign.center,
                     textStyle: const TextStyle(
                       fontFamily: 'FeatureMono',
                       fontSize: 50,
                     ),
-                    waveColor: const Color(0xffe6f032),
+                    waveColor: fontYellow,
                     boxBackgroundColor: Colors.blueGrey,
                   ),
                   onTap: () => {
